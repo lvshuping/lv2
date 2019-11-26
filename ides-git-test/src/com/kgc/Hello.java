92df85aaf00cbddafc79c2a521c0f7d228d31d4e
lsp <1925259616@qq.com>
2019-11-26 15:14:47 +0800

    添加到本地仓库

---
+++ ides-git-test/src/com/kgc/Hello.java
@@ -0,0 +1,10 @@
+package com.kgc;
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("提交");
+        System.out.println("提交");
+        System.out.println("提交");
+        System.out.println("提交");
+    }
+}
